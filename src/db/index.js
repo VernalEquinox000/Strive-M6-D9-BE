da284c43aab30adfd78ba4ff2ea38cc8a35a0181
VernalEquinox <pieroapruzzese@gmail.com>
2021-01-21 16:51:42 +0100

    working on models

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require("sequelize")
+const Article = require("./article")
+const sequelize = new Sequelize (process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    })
+
+    //once model is created
+const models = {
+        Article: Article(sequelize,DataTypes)
+    }
+  
+    sequelize
+  .authenticate()
+  .then(() => console.log("Connection established"))
+  .catch((e) => console.log("Connection failed ", e));
+//from sequelize getting started doc + test Connection
+
+module.exports = models;
+
+
